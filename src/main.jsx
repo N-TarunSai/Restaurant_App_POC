49cd0031962376889d0261cd3198e3cc821154e7
N <tarunsai.n@innovasolutions.com>
2025-09-29 11:32:04 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store/store'
+import { Analytics } from "@vercel/analytics/react";
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
